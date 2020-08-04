b587c3c6c71e04bc3f77247bb2f33686d78b2476
Jesse Audije <audijej@gmail.com>
2020-08-04 00:15:37 -0700

    mongo test 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-let MONGODB_URI = "mongodb://<dbuser>:<dbpassword>@ds037758.mlab.com:37758/heroku_xzzcp857"
+// let MONGODB_URI = "mongodb://<dbuser>:<dbpassword>@ds037758.mlab.com:37758/heroku_xzzcp857"
 
 const PORT = 9000;
 
@@ -16,12 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {useMongoClient: true})
+// mongoose.connect(MONGODB_URI, {useMongoClient: true})
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds037758.mlab.com:37758/heroku_xzzcp857", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
